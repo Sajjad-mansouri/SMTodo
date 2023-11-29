b97ba0e945df82dc76a07df2991c008799d3f2e3
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-11-29 15:33:52 +0330

    create model for todo app

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+@admin.register(Todo)
+class ToDoAdmin(admin.ModelAdmin):
+
+	list_filter=['priority','user','alarm']
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Todo(models.Model):
+	PRIORITY=[
+	('h','High'),
+	('m','Medium'),
+	('l','Low'),
+	]
+	user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='todos')
+	text=models.TextField()
+	alarm=models.DateTimeField(blank=True,null=False)
+	created=models.DateTimeField(auto_now_add=True)
+	updated=models.DateTimeField(auto_now=True)
+	priority=models.CharField(choices=PRIORITY,default='m',max_length=1,null=True)
+
+
