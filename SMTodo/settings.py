be697069151a674869bf4473e099a6bb5718c808
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-12-08 21:32:16 +0330

    add allowed host to env

--- SMTodo/settings.py
+++ SMTodo/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['localhost']
 
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 # Application definition
 
